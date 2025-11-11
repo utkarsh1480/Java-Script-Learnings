cb2007cda8dac781cf9144407bae8c9e380b4b39
UTKARSH <utkarsh1480759@gmail.com>
2025-11-12 00:47:55 +0530

    Practice of JS object Mathods

--- 02Basics/object.js
+++ 02Basics/object.js
@@ -25,9 +25,35 @@ let jsUser = {
 // console.log(typeof keys[0]);
 // console.log(typeof jsUser.sym01) ;
 
+// jsUser.greeting = function(){
+//     console.log(`Hello there! ${this.name}`);
+// }
+// console.log(jsUser.greeting);
+// console.log(jsUser.greeting());
+
+// console.log(Object.keys(jsUser)); // get all keys of object in array form
+// const allKeys = Reflect.ownKeys(jsUser); // get all keys including symbol keys
+// console.log(allKeys.length); // 6
+// jsUser.isActive = false; // updating existing key value
+// console.log(jsUser); // false
+// Object.freeze(jsUser); // freeze the object
+// jsUser.course = "Python"; // trying to update after freezing
+// console.log(jsUser); // still JavaScript
 jsUser.greeting = function(){
     console.log(`Hello there! ${this.name}`);
 }
-console.log(jsUser.greeting);
-console.log(jsUser.greeting());
+console.log(jsUser); // no greeting method added
+console.log(jsUser.greeting); // undefined
+console.log(jsUser.greeting()); // error jsUser.greeting is not a function
 
+//Some Methods of object
+// console.log(Object.keys(jsUser)); // get all keys of object in array form
+// const allKeys = Reflect.ownKeys(jsUser); // get all keys including symbol keys
+// console.log(allKeys.length); // 6
+// console.log(Object.values(jsUser)); // get all values of object in array form
+// console.log(Object.entries(jsUser)); // get all entries of object in array form as [key,value] pairs
+// console.log(jsUser.hasOwnProperty("name")); // true
+// console.log(jsUser.hasOwnProperty("nonExistingKey")); // false
+// console.log(jsUser.isPrototypeOf(jsUser)); // false
+// const newUser = Object.assign({}, jsUser); // shallow copy of object
+// console.log(newUser);
